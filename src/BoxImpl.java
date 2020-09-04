52ce58412154e67d0ed58e8a28c7b91da7ff471e
SelinM <maksimselin108@gmail.com>
2020-09-04 15:48:21 +0300

    edit 3

--- src/BoxImpl.java
+++ src/BoxImpl.java
@@ -1,4 +1,4 @@
-public class BoxImpl implements Box{
+public class BoxImpl implements Box {
 
     private int size;
     private Sweetness[] candyBox;
@@ -7,31 +7,26 @@ public class BoxImpl implements Box{
 
     @Override
     public void addByIndex(int index, Sweetness sweetness) {
-        if (numberOfSweetness < size) {
-            if (index < size) {
-                if (candyBox[index] == null) {
-                    candyBox[index] = sweetness;
-                    numberOfSweetness++;
-                    if (maxIndex < index) maxIndex = index;
-                    System.out.println("position " + index + " filled");
-                } else System.out.println("position " + index + " already filled");
-            } else System.out.println("Index is out of range");
-        }
-        else System.out.println("overflow");
+        if (index < size) {
+            if (candyBox[index] == null) {
+                candyBox[index] = sweetness;
+                numberOfSweetness++;
+                if (maxIndex < index) maxIndex = index;
+                System.out.println("position " + index + " filled");
+            } else System.out.println("position " + index + " already filled");
+        } else System.out.println("Index is out of range");
+
     }
 
     @Override
     public void addLast(Sweetness sweetness) {
-        if (numberOfSweetness==0) maxIndex = -1;
-        if (numberOfSweetness < size) {
-            if (maxIndex < size - 1) {
-                candyBox[maxIndex + 1] = sweetness;
-                maxIndex++;
-                numberOfSweetness++;
-                System.out.println("position " + maxIndex + " filled");
-            } else System.out.println("overflow");
-        }
-        else System.out.println("overflow");
+        if (numberOfSweetness == 0) maxIndex = -1;
+        if (maxIndex < size - 1) {
+            candyBox[maxIndex + 1] = sweetness;
+            maxIndex++;
+            numberOfSweetness++;
+            System.out.println("position " + maxIndex + " filled");
+        } else System.out.println("overflow");
     }
 
     @Override
@@ -42,12 +37,15 @@ public class BoxImpl implements Box{
                     candyBox[index] = null;
                     System.out.println("sweetness by index: " + index + " deleted");
                     numberOfSweetness--;
-                    if (numberOfSweetness == 0) return;
-                    if (index == maxIndex) {
-                        for (int i = maxIndex - 1; i >= 0; i--) {
-                            if (candyBox[i] != null) {
-                                maxIndex = i;
-                                break;
+                    if (numberOfSweetness == 0) {
+                        maxIndex = -1;
+                    } else {
+                        if (index == maxIndex) {
+                            for (int i = maxIndex - 1; i >= 0; i--) {
+                                if (candyBox[i] != null) {
+                                    maxIndex = i;
+                                    break;
+                                }
                             }
                         }
                     }
@@ -57,58 +55,58 @@ public class BoxImpl implements Box{
     }
 
     @Override
-    public void deleteLast(){
-        if (numberOfSweetness!=0){
+    public void deleteLast() {
+        if (numberOfSweetness != 0) {
             candyBox[maxIndex] = null;
             numberOfSweetness--;
             System.out.println("sweetness by index: " + maxIndex + " deleted");
-            if (numberOfSweetness==0) return;
-            else for (int i = maxIndex - 1; i >= 0; i--) {
+            if (numberOfSweetness == 0) {
+                maxIndex = -1;
+                return;
+            } else for (int i = maxIndex - 1; i >= 0; i--) {
                 if (candyBox[i] != null) {
                     maxIndex = i;
                     break;
                 }
             }
 
-        }
-        else System.out.println("Box is empty");
+        } else System.out.println("Box is empty");
     }
 
     @Override
     public int weight() {
         int weightBox = 0;
-        if (numberOfSweetness !=0) {
-            for (int i = 0; i <= maxIndex; i++){
-                if (candyBox[i]!= null)
+        if (numberOfSweetness != 0) {
+            for (int i = 0; i <= maxIndex; i++) {
+                if (candyBox[i] != null)
                     weightBox = weightBox + candyBox[i].getWeight();
             }
             return weightBox;
-        }else return weightBox;
+        } else return weightBox;
     }
 
     @Override
     public int price() {
         int priceBox = 0;
-        if (numberOfSweetness !=0){
-            for (int i = 0; i <= maxIndex; i++){
-                if (candyBox[i]!=null)
+        if (numberOfSweetness != 0) {
+            for (int i = 0; i <= maxIndex; i++) {
+                if (candyBox[i] != null)
                     priceBox = priceBox + candyBox[i].getPrice();
             }
             return priceBox;
-        }else return priceBox;
+        } else return priceBox;
     }
 
     @Override
     public void info() {
-        if (numberOfSweetness !=0){
+        if (numberOfSweetness != 0) {
             System.out.println("=================================");
-            for (int i = 0; i < size; i++){
-                if (candyBox[i]!=null) System.out.println("position " + i + " " + candyBox[i]);
-                else System.out.println("position "+ i +" is empty");
+            for (int i = 0; i < size; i++) {
+                if (candyBox[i] != null) System.out.println("position " + i + " " + candyBox[i]);
+                else System.out.println("position " + i + " is empty");
             }
             System.out.println("=================================");
-        }
-        else System.out.println("box is empty");
+        } else System.out.println("box is empty");
     }
 
     public BoxImpl(int size) {
@@ -116,25 +114,24 @@ public class BoxImpl implements Box{
         candyBox = new Sweetness[size];
     }
 
-    public void optimizedByWeight(int weight){
-        if (weight()>=weight){
+    public void optimizedByWeight(int weight) {
+        if (weight() >= weight) {
             deleteByIndex(minIndexWeight());
             optimizedByWeight(weight);
-        }
-        else System.out.println("Box does not need optimization");
+        } else System.out.println("Box does not need optimization");
     }
 
-    public int minIndexWeight(){
+    public int minIndexWeight() {
         int index = 0;
         int min = 0;
-        for (int i = 0; i <= maxIndex; i++){
-            if (candyBox[i]!= null){
+        for (int i = 0; i <= maxIndex; i++) {
+            if (candyBox[i] != null) {
                 index = i;
                 min = candyBox[i].getWeight();
                 break;
             }
         }
-        if(index < maxIndex) {
+        if (index < maxIndex) {
             for (int i = index + 1; i <= maxIndex; i++) {
                 if (candyBox[i] != null) {
                     if (candyBox[i].getWeight() < min) {
@@ -147,25 +144,24 @@ public class BoxImpl implements Box{
         return index;
     }
 
-    public void optimizedByPrice(int price){
-        if (price()>=price){
+    public void optimizedByPrice(int price) {
+        if (price() >= price) {
             deleteByIndex(minIndexPrice());
             optimizedByPrice(price);
-        }
-        else System.out.println("Box does not need optimization");
+        } else System.out.println("Box does not need optimization");
     }
 
-    public int minIndexPrice(){
+    public int minIndexPrice() {
         int index = 0;
         int min = 0;
-        for (int i = 0; i <= maxIndex; i++){
-            if (candyBox[i]!= null){
+        for (int i = 0; i <= maxIndex; i++) {
+            if (candyBox[i] != null) {
                 index = i;
                 min = candyBox[i].getPrice();
                 break;
             }
         }
-        if(index < maxIndex) {
+        if (index < maxIndex) {
             for (int i = index + 1; i <= maxIndex; i++) {
                 if (candyBox[i] != null) {
                     if (candyBox[i].getPrice() < min) {
